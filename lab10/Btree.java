0328aff446f20311748212308c7897ddf9266156
maldest <andre.eguia01@gmail.com>
2025-06-24 11:57:10 -0500

    Update Btree.java

---
+++ lab10/Btree.java
@@ -0,0 +1,15 @@
+package lab10;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+/**
+ * Implementación de un B-Tree genérico
+ * @param <E> Tipo de elementos almacenados en el árbol, debe ser Comparable
+ */
+
+ public class BTree<E extends Comparable <E>>{
+    private BNode<E> root;
+    priva
+ }
